58fbca749034a6edb09fe081ecd091d57511b128
Mark Desnoyer <desnoyer@neon-lab.com>
2013-08-09 13:05:48 -0700

    Posting job uses S3 and will post in the same mturk group now

--- post_hits.rb
+++ post_hits.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'rubygems'
+require 'aws-sdk'
 require 'rturk'
 require 'fileutils'
 require 'csv'
@@ -9,16 +11,24 @@ require 'trollop'
 $STAGING_APP='gentle-escarpment-8454-staging'
 $PROD_APP='gentle-escarpment-8454'
 
+$AWS_ACCESS_KEY='AKIAJ5G2RZ6BDNBZ2VBA'
+$AWS_SECRET_KEY='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'
+AWS.config(
+  :access_key_id => $AWS_ACCESS_KEY, 
+  :secret_access_key => $AWS_SECRET_KEY
+)
+
 def all_children_except(parent_folder, extra_regex = '')
     regex = '^\.'
     regex += '|' + extra_regex unless extra_regex.empty?
     Dir.entries(parent_folder).reject { |file| file =~ Regexp.new(regex) }.map { |file| File.expand_path(file, parent_folder) }
 end
 
-def post_task(unique_id, folder_name, hit_title, payment_amount, hit_assignments, sandbox)
+def post_tasks(job_names, bucket_name, payment_amount, hit_assignments,
+               sandbox)
 
-  RTurk.setup("AKIAJ5G2RZ6BDNBZ2VBA",
-              "d9Q9abhaUh625uXpSrKElvQ/DrbKsCUAYAPaeVLU",
+  RTurk.setup($AWS_ACCESS_KEY,
+              $AWS_SECRET_KEY,
               :sandbox => sandbox)
 
   if sandbox then
@@ -27,20 +37,30 @@ def post_task(unique_id, folder_name, hit_title, payment_amount, hit_assignments
       app_base=$PROD_APP
   end
   url_base = "http://#{app_base}.herokuapp.com"
-  
-  hit = RTurk::Hit.create(:title => "Choose an Online a Video! [#{unique_id}]") do |hit|
-    hit.assignments = hit_assignments
+
+  hittype = RTurk::RegisterHITType.create(:title => "Choose an Online a Video!") do |hit|
     hit.description = 'Choose an Online a Video!'
-      hit.question("#{url_base}/keep_instructions/#{folder_name}", :frame_height => 1000)
-    hit.question("My Survey")
     hit.reward = payment_amount
-    hit.lifetime = 86400
-    hit.qualifications.add :approval_rate, { :gt => 80 }
+    hit.duration = 14400
     hit.keywords = "images, game, psychology, video, fast"
+    hit.qualifications.add :approval_rate, { :gt => 80 }
+  end
+
+  puts hittype.type_id
+  
+  job_names.each do |job_name|
+    hit = RTurk::Hit.create() do |hit|
+      hit.assignments = hit_assignments
+      hit.hit_type_id = hittype.type_id
+      hit.lifetime = 2592000
+      hit.question("#{url_base}/keep_instructions/#{bucket_name}",
+                   :job => job_name,
+                   :frame_height => 1000)
+    end
   end
   
   # Clipboard.copy hit.url
-  puts "Job id: #{unique_id} on mturk at: #{hit.url}"
+  puts "Job on mturk at: #{hit.url}"
 end
 
 # Define the command line arguments
@@ -50,15 +70,9 @@ opts = Trollop::options do
   opt :assignments, "Number of assignments", :type => :int, :default =>1
   opt :input, "List of directories containing new stimuli sets",
     :type => :strings
-  opt :output, "Path to the app. Defaults to the current directory", 
-    :type => :string
+  opt :s3bucket, "Bucket name to host the images", :type => :string,
+    :default => "mturk_bday_thumbs_9lwe9"
 end
-
-# Default the app dir to the current directory
-if opts[:output].nil?
-  opts[:output] = Dir.pwd
-end
-Dir.chdir(opts[:output])
 
 image_sets = []
 opts[:input].each do |folder|
@@ -67,66 +81,42 @@ opts[:input].each do |folder|
 end
 
 image_sets.each do |folder|
+  unique_id = SecureRandom.hex(10)
+  folder[:unique_id] = unique_id
+  folder[:new_name] = "#{folder[:folder_name]}_#{unique_id}"
 
-  # Get the list of images in the folder
+  # Upload the images to the S3 bucket
+  puts("Uploading images from #{folder[:original_path]} to S3 bucket #{opts[:s3bucket]}")
+  s3 = AWS::S3.new
   folder[:file_names] = []
   all_image_paths = all_children_except(folder[:original_path])
-  all_image_paths.each do |image|
-    folder[:file_names] << File.basename(image)
+  all_image_paths.each do |image_path|
+    key = File.basename(image_path)
+    folder[:file_names] << key
+    s3obj = s3.buckets[opts[:s3bucket]].objects[key]
+    if !s3obj.exists? then
+        s3obj.write(:file => image_path)
+    end
   end
-  
-  unique_id = SecureRandom.hex(10)
-
-  # New images folder name
-  folder[:new_name]  = "#{folder[:folder_name]}_#{unique_id}"
-  folder[:unique_id] = unique_id
-
-  puts "Creating new stimuli set from #{folder[:original_path]} as #{folder[:new_name]}"
-
-  # New images folder complete path
-  images_path = "#{opts[:output]}/app/assets/images"
-  images_folder_path = "#{images_path}/#{folder[:new_name]}"
-
-  # Copy folder from original location into main images folder in app
-  FileUtils.cp_r(folder[:original_path], images_path,
-                 :remove_destination => true)
-  FileUtils.mv("#{images_path}/#{folder[:folder_name]}",
-               images_folder_path)
-  `git add #{images_folder_path}`
 
   # Create the text file for this stimset
-  text_files_main = "#{opts[:output]}/config"
-  folder[:text_file_folder] = "#{text_files_main}/#{folder[:new_name]}"
-  FileUtils.mkdir_p("#{folder[:text_file_folder]}")
-  folder[:text_file_path]   = "#{folder[:text_file_folder]}/stimuli.csv"
-  CSV.open(folder[:text_file_path], "w") do |csv|
+  text_file_name = "#{folder[:new_name]}_stimuli.csv"
+  csv_string = CSV.generate do |csv|
     (1..108).each do |i|
       csv << ["#{i}", "#{folder[:file_names][i-1]}"]
     end
   end
 
-  `git add #{folder[:text_file_path]}`
-end
+  puts("Writing #{text_file_name} to S3")
+  s3.buckets[opts[:s3bucket]].objects[text_file_name].write(csv_string)
 
-if opts[:sandbox] then
-    app_base=$STAGING_APP
-else
-    app_base=$PROD_APP
-end
-git_url = "git@heroku.com:#{app_base}.git"
-
-# Deploy the app to Heroku
-if ! system("git commit -m 'Updating stimuli sets'") then
-    puts("Error commiting to the git repo")
-    exit(1)
-end
-if ! system("git push #{git_url} master") then
-    puts("Error deploying to Heroku")
-    exit(1)
 end
 
 # Post HITs
+job_names = []
 image_sets.each do |set|
-  post_task(set[:unique_id], set[:new_name], set[:hit_title], opts[:pay],
-            opts[:assignments], opts[:sandbox])
+  job_names << set[:new_name]
 end
+post_tasks(job_names, opts[:s3bucket], opts[:pay], opts[:assignments],
+           opts[:sandbox])
+
