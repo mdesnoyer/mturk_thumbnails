3cfa342fb273daa0fc9d917f36f77a4ad43f45b5
Mark Desnoyer <desnoyer@neon-lab.com>
2014-01-30 12:13:23 -0800

    Fix migration

--- db/migrate/014_add_timestamps_to_user_rejections.rb
+++ db/migrate/014_add_timestamps_to_user_rejections.rb
@@ -1,5 +1,5 @@
 class AddTimestampsToUserRejections < ActiveRecord::Migration
-    def change_table
+    def change
       add_column(:user_rejections, :created_at, :datetime)
       add_column(:user_rejections, :updated_at, :datetime)
     end
